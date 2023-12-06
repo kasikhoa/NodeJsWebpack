4dce530c3a07b460a6df7e0df4dcb3b6e50b2d3c
kasikhoa <kasikhoa@gmail.com>
2023-12-06 23:27:26 +0700

    use babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,49 +3,64 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = (env) => {
-    const isDevelopment = process.env.NODE_ENV === 'development'
-    return {
-        mode: isDevelopment ? 'development' : 'production',
-        entry: {
-            app: path.resolve('src/index.js'),
+  const isDevelopment = process.env.NODE_ENV === 'development'
+  return {
+    mode: 'development',
+    entry: {
+      app: path.resolve('src/index.js')
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      clean: true
+    },
+    devtool: isDevelopment ? 'source-map' : false,
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss|css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
         },
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: '[name].[contenthash].js',
-            clean: true,
-        },
-        devtool: isDevelopment ? 'source-map' : false,
-        module: {
-            rules: [
-                {
-                    test: /\.s[ac]ss|css$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        'css-loader',
-                        'sass-loader',
-                    ],
-                },
-            ],
-        },
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: '[name].[contenthash].css',
-            }),
-            new HtmlWebpackPlugin({
-                title: 'Webpack App',
-                filename: 'index.html',
-                template: 'src/template.html',
-            }),
-        ],
-        devServer: {
-            static: {
-                directory: 'dist', // Đường dẫn tương đối đến với thư mục chứa index.html
-            },
-            port: 3000, // Port thay cho port mặc định (8080)
-            open: true, // Mở trang webpack khi chạy terminal
-            hot: true, // Bật tính năng reload nhanh Hot Module Replacement
-            compress: true, // Bật Gzip cho các tài nguyên
-            historyApiFallback: true, // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
-        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    debug: true, // Hiển thị debug lên terminal để dễ debug
+                    useBuiltIns: 'entry',
+                    corejs: '3.23.4' // nên quy định verson core-js để babel-preset-env nó hoạt động tối ưu
+                  }
+                ]
+              ]
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      }),
+      new HtmlWebpackPlugin({
+        title: 'Webpack App',
+        filename: 'index.html',
+        template: 'src/template.html'
+      })
+    ],
+    devServer: {
+      static: {
+        directory: 'dist' // Đường dẫn tương đối đến với thư mục chứa index.html
+      },
+      port: 3000, // Port thay cho port mặc định (8080)
+      open: true, // Mở trang webpack khi chạy terminal
+      hot: true, // Bật tính năng reload nhanh Hot Module Replacement
+      compress: true, // Bật Gzip cho các tài nguyên
+      historyApiFallback: true // Set true nếu bạn dùng cho các SPA và sử dụng History API của HTML5
     }
+  }
 }
